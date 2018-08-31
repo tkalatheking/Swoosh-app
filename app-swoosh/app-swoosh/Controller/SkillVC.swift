405b6a28fd364b8e89c9bcd162eeb3c29c0d3c01
Marko Tkalcic <tkalcic.marko@gmail.com>
2018-08-31 18:17:05 +0530

    Refactroing in Xcode

---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Marco Tkalcic on 8/31/18.
+//  Copyright © 2018 Marco Tkalcic. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
