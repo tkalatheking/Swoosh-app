225f25b066e2996a7421ded9c649b3ffcf874304
Marko Tkalcic <tkalcic.marko@gmail.com>
2018-08-31 19:57:38 +0530

    passing data betwen VC

--- app-swoosh/app-swoosh/Controller/LeaugeVC.swift
+++ app-swoosh/app-swoosh/Controller/LeaugeVC.swift
@@ -43,14 +43,10 @@ class LeaugeVC: UIViewController {
         NextBtn.isEnabled = true
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
-    */
 
 }
